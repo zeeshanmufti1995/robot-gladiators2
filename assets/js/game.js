80bc5990db9a4fa807c37f34db49b172741a92ce
Zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-06-02 07:38:36 -0500

    functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = prompt("What is your Robot Name?");
+    console.log(playerName)
+function fight(){
+    alert("Fight has begun");
+}
+//fight();
